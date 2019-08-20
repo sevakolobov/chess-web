b16e9d8e4b10be5182939e1db32deead4a27eb66
Vsevolod Kolobov <vsevolok.atribut@gmail.com>
2019-08-20 20:03:21 +0300

    launched localhost

---
+++ src/main/java/net/kolobov/chessweb/ChessWebApplication.java
@@ -0,0 +1,22 @@
+package net.kolobov.chessweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class ChessWebApplication  implements WebMvcConfigurer {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessWebApplication.class, args);
+
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+
+
+}
---
+++ src/main/java/net/kolobov/chessweb/controller/MainController.java
@@ -0,0 +1,12 @@
+package net.kolobov.chessweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = "/")
+    public String getIndex()  {
+        return "index.html";
+    }
+}
